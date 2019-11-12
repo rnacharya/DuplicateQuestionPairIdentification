929b3879c9fdc6123421927048c7a89bf5cca8dd
Rachana Narayan Acharya <rachananarayanacharya@1x-nat-vl930-172-30-79-98.wireless.umass.edu>
2019-11-12 17:02:40 -0500

    Data preprocessing-Adding non duplicate pairs

--- preprocess_stackexchange.py
+++ preprocess_stackexchange.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import xml.etree.ElementTree as et
+from random import randint
 
 xtree = et.parse("/Users/rachananarayanacharya/Downloads/Posts.xml")
 xroot = xtree.getroot()
@@ -20,7 +21,9 @@ for node in xroot:
                  "Body": body, "Title": title})
 
 #print(rows)
+
 posts = pd.DataFrame(rows, columns = df_cols)
+posts = posts[posts["PostTypeId"] == "1"]
 
 posts["Body"]=posts["Title"]+posts["Body"]
 #print(posts["Body"].head(5))
@@ -41,13 +44,58 @@ for node in xroot:
     rows.append({"PostId": id, "RelatedPostId": rid})
 
 postLinks = pd.DataFrame(rows, columns = df_cols)
-merged_inner_1=pd.merge(left=posts,right=postLinks, left_on='Id', right_on='PostId')
+merged_inner=pd.merge(left=posts,right=postLinks, left_on='Id', right_on='PostId')
 
-merged_inner_2=pd.merge(left=merged_inner_1,right=posts, left_on='RelatedPostId', right_on='Id')#body_x body_y
+combined_df=pd.merge(left=merged_inner,right=posts, left_on='RelatedPostId', right_on='Id')#body_x body_y
 
 
-merged_inner_2.drop(['PostTypeId_y', 'PostTypeId_x','Id_y', 'Id_x','Title_x', 'Title_y'], axis=1, inplace=True)
-for column in merged_inner_2.columns:
-    print(merged_inner_2[column].head(5))
-#merged_inner_2.drop(['PostTypeId_y', 'PostTypeId_x','Id_y', ' Id_x','Title_x', 'Title_y', ], axis=1)
-#merged_inner_2.drop(['B', 'C'], axis=1)
+combined_df.drop(['PostTypeId_y', 'PostTypeId_x','Id_y', 'Id_x','Title_x', 'Title_y'], axis=1, inplace=True)
+combined_df.rename(columns={"PostId":"qid1", "Body_x": "question1", "RelatedPostId":"qid2", "Body_y": "question2"}, inplace=True)
+combined_df.index.names = ['id']
+
+combined_df["is_duplicate"]=1
+
+#print("hii ",combined_df.shape)
+#print("hii-1 ",combined_df.head())
+# print("hii-1 ",combined_df[1])
+
+pairs_set=set()
+for idx, row in combined_df.iterrows():
+    #print("hey", row)
+    # print("hey1 ", row['qid1'])
+
+    q1=row['qid1']
+    q2=row['qid2']
+    qpair=""
+
+    if q1<q2:
+        qpair=str(q1)+" "+str(q2)
+    else:
+        qpair=str(q2)+" "+str(q1)
+    pairs_set.add(qpair)
+    #print(qpair)
+cur_size, cur_cols=combined_df.shape
+print("initial rows:", cur_size)
+for index in range(0, cur_size):
+    qpair=""
+    while(True):
+        id1=combined_df.iloc[randint(0, cur_size-1)].loc["qid1"]#["qid1"]
+        id2=combined_df.iloc[randint(0, cur_size-1)].loc["qid2"]
+        qpair=str(id1)+" "+str(id2)
+        if(not qpair in pairs_set and (not id1==id2)):
+            break
+    #print(qpair)
+
+    post1=posts.loc[posts['Id'] == id1]
+    #print(post1["Body"].item())
+    #raise SystemExit
+    body1=post1["Body"].item()
+
+    post2=posts.loc[posts['Id'] == id2]
+    body2=post2["Body"].item()
+    df_entry={"qid1":id1, "question1": body1,"qid2":id2, "question2": body2, "is_duplicate":0}
+    combined_df=combined_df.append(df_entry, ignore_index=True)
+print("Current size",combined_df.shape)
+combined_df.to_csv('stack_exchange.csv')
+    #print(post1)
+    #body1=post1["Body"]
